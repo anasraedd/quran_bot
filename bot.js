045e23c2d84ef8b3233494abcab613e51f1fac0c
anas raed al-drfeel <95145504+anasraedd@users.noreply.github.com>
2026-01-17 18:01:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -161,7 +161,7 @@ function saveAchievement(ctx, data) {
 }
 
 /* ================== التقييم ================== */
-bot.action(/rate_(\d+)/, ctx => {
+bot.action(rate_${id})/, ctx => {
   const id = ctx.match[1]
   ctx.editMessageReplyMarkup(
     Markup.inlineKeyboard([
@@ -230,3 +230,4 @@ bot.action(/send_(\d+)/, ctx => {
 
 bot.launch()
 console.log('Bot running...')
+
